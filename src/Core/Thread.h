5fccb86d6d94eae3860d0770a8a3c495d78b77cf
VersuchDrei <FalkoMitC@gmail.com>
2022-12-13 13:43:53 +0100

    added anim event listening

--- src/Core/Thread.h
+++ src/Core/Thread.h
@@ -6,9 +6,11 @@
 namespace OStim {
 
     using ThreadId = int64_t;
-    class Thread {
+    class Thread : public RE::BSTEventSink<RE::BSAnimationGraphEvent>{
     public:
         Thread(ThreadId a_id, std::vector<RE::Actor*> a_actors);
+
+        ~Thread();
         
         void ChangeNode(Graph::Node* a_node);
 
@@ -22,6 +24,8 @@ namespace OStim {
 
         void SetSpeed(int speed) { m_currentNodeSpeed = speed; }
 
+        virtual RE::BSEventNotifyControl ProcessEvent(const RE::BSAnimationGraphEvent* a_event, RE::BSTEventSource<RE::BSAnimationGraphEvent>* a_eventSource) override;
+
     private:
         ThreadId m_threadId;        
         std::map<int32_t, ThreadActor> m_actors;
@@ -29,6 +33,8 @@ namespace OStim {
         Graph::Node* m_currentNode = nullptr;
         int m_currentNodeSpeed = 0;        
         std::thread m_excitementThread;
+        void addActorSink(RE::Actor* a_actor);
+        void removeActorSink(RE::Actor* a_actor);
     };
 
 }  // namespace OStim
